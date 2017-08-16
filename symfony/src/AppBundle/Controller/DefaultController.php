79c0fba1f4062904f1bc5b7fbf7f1bc71e93a406
samu <torres.88.bcn@gmail.com>
2017-08-16 18:05:46 +0200

    first commit

---
+++ symfony/src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use BackendBundle\Entity\Block;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Services\Helpers;
+
+class DefaultController extends Controller {
+
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request) {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+                    'base_dir' => realpath($this->getParameter('kernel.project_dir')) . DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/pruebas", name="pruebas", methods="GET")
+     */
+    public function pruebasAction(Request $request, Helpers $helpers) {
+                
+        $em = $this->getDoctrine()->getManager();
+        $blocks = $em->getRepository(Block::class)->findAll();
+
+        $response = $helpers->toJson($blocks);
+        return $response;
+        
+    }
+
+}
